0b4bc0d5e4c998f23a97e13ad35827f0c1efb69b
Mins97 <tkarnrwl7862@naver.com>
2021-10-30 00:07:52 +0900

    Updates

--- src/components/Input/CommandInput/CContour.js
+++
@@ -1,7 +0,0 @@
-import { SCREEN_WIDTH } from '../../../style/index';
-
-export const CContour = ({ index, data }) => {
-  return (
-    <div style={{ width: SCREEN_WIDTH * 0.8, height: 2, backgroundColor:'black', marginTop:11,borderRadius:10 }}/>
-  );
-};
