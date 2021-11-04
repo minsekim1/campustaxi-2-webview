dbe34de29d0b0576e5ec359d1ed24cf82337910f
Mins97 <tkarnrwl7862@naver.com>
2021-11-05 02:01:16 +0900

    textarea color 1차완료

--- src/components/Input/CommandInput/CTextarea.js
+++ src/components/Input/CommandInput/CTextarea.js
@@ -1,23 +1,25 @@
-import { GRAY7, GRAY8, SCREEN_WIDTH } from "../../../style";
+import { GRAY6, GRAY7, GRAY8, SCREEN_WIDTH } from "../../../style";
 import { useState, useCallback, useRef, useEffect } from "react";
 import TextareaAutosize from "react-autosize-textarea";
 import { offset, position } from "caret-pos";
 import { useRecoilState } from "recoil";
 import { commandInputListState, commandWindowState } from "./../../recoil";
 import { getPlatform } from "../../common/function/getPlatform";
+import _ from "lodash";
 export const platform = getPlatform();
 
 export const CTextarea = ({ style, maxrows, index }) => {
   const [placeholder, setPlaceHolder] = useState("");
   const [rowIndex, setRowIndex] = useState({ height: 0, index: 0 });
-  const [beforeInput, setBeforeInput] = useState("");
   const [commandWindow, setCommandWindow] = useRecoilState(commandWindowState);
   const [commandInputList, setCommandInputList] = useRecoilState(commandInputListState);
 
   const ref = useRef();
+  // 텍스트 변한 위치 찾기
+  const beforeInput = useRef("");
   //#region 컬러선택시 특정위치에 컬러넣기
   //index -> -1 필요
-  let codes = "<b>Will This Work?</b>";
+  let codes = "";
   useEffect(() => {
     if (!!commandWindow.color && commandWindow.index === index) {
       let color = GRAY8;
@@ -47,53 +49,32 @@ export const CTextarea = ({ style, maxrows, index }) => {
         default:
           break;
       }
-      let origin = ref.current.innerHTML;
-      let split = typeof origin === "string" ? origin.split(/(?:<|>)+/) : "";
-      let left = "";
-      let right = "";
-      if (split.length === 5) {
-        left = split[0] + "<" + split[1] + ">" + split[2].slice(0, commandWindow.pos - 1);
-        right = split[2].slice(commandWindow.pos, origin.length) + "<" + split[3] + ">" + split[4];
-      } else {
-        let p = commandWindow.pos;
-        let i;
-        try {
-          // 배열 번호(p)와 문자열 번호 구하기(i)
-          for (i = 0; split[i].length < p; i++) p -= split[i].length;
-          // console.log(p,i) index:2,pos:3
-          // left 구하기
-          for (let i = 0; i <= p; i++) left += split[i] + (i % 2 === 0 ? "<" : ">");
-          left += split[p + 1].slice(0, i);
-          // right 구하기
-          right += split[p + 1].slice(i + 1, split[p].length);
-          for (let i = p + 1; i < split[i].length + 2; i++) {
-            right += (i % 2 === 0 ? "<" : ">") + split[i + 1];
-            console.log(split[i + 1], i % 2 === 0 ? "<" : ">");
-          }
-          right += ">"
-        } catch (e) {
-          console.debug(e);
-        }
-      }
-      console.log("left", left, "right", right);
+      const origin = ref.current.innerHTML;
+      const left = origin.slice(0, commandWindow.pos);
+      const right = origin.slice(commandWindow.pos, origin.length);
       ref.current.innerHTML = `${left}<span style='color:${color};background-color:${backgroundColor}'>${commandWindow.color}</span>${right}`;
-      // if (origin.length >= commandWindow.pos) {
-      //   const left = origin.slice(0, commandWindow.pos);
-      //   const right = origin.slice(commandWindow.pos, origin.length);
-      //   console.log("origin", origin, left, right);
-      //   ref.current.innerHTML = `${left}<span style='color:${color};background-color:${backgroundColor}'>${commandWindow.color}</span>${right}`;
-      // }
     }
   }, [commandWindow.color]);
   //#endregion
   const onInput = useCallback(
     (e) => {
+      //#region placeholer 삭제
+      if(placeholder !== "" &&e.target.innerHTML !=="" ) setPlaceHolder("")
+      //#endregion
+      //#region 추가된 텍스트 위치 찾기 (pos)
+      let pos;
+      const len =
+        beforeInput.current.length > e.target.innerHTML.length ? beforeInput.current.length : e.target.innerHTML.length;
+      for (pos = 0; pos < len; pos++) {
+        if (beforeInput.current[pos] !== e.target.innerHTML[pos]) break;
+      }
+      //#endregion
       //#region #누르면 명령어창 보여줌 || 안드로이드 설정(한번에 여러개들어옴)
       if (
         e.nativeEvent.data == "#" ||
         (platform === "android" && !!e.nativeEvent.data && e.nativeEvent.data.includes("#"))
       ) {
-        let { left, top, height, pos } = position(ref.current);
+        let { left, top, height } = position(ref.current);
         //#region 가로이동 제한 & height 화면 안 넘어가게 제한
         // 가로제한
         if (left + 260 > SCREEN_WIDTH && left < 260) {
@@ -119,15 +100,14 @@ export const CTextarea = ({ style, maxrows, index }) => {
         setCommandWindow({ ...commandWindow, visible: false, index: -1 });
       }
       //#region 내용업데이트
+      beforeInput.current = e.target.innerHTML;
       setCommandInputList([
         ...commandInputList.slice(0, index),
-        { ...commandInputList[index], content: e.target.value },
+        { ...commandInputList[index], content: e.target.innerHTML },
         ...commandInputList.slice(index + 1, 999),
       ]);
       //#endregion
-      setBeforeInput(e.target.value);
       //#endregion #누르면 명령어창 보여줌
-
       //#region 줄 수 제한 높이체크
       if (!!maxrows) {
         if (rowIndex.index < maxrows && rowIndex.height < e.target.scrollHeight)
@@ -138,17 +118,36 @@ export const CTextarea = ({ style, maxrows, index }) => {
       }
       //#endregion 줄 수 제한 높이체크
     },
-    [rowIndex, beforeInput, commandWindow, index]
+    [rowIndex, beforeInput.current, commandWindow, index]
   );
   const onBlur = () => {
     setPlaceHolder("");
   };
   const onFocus = () => {
-    if (commandWindow.index != index) {
-      if (commandWindow.visible) setCommandWindow({ ...commandWindow, visible: false, index: index });
-      else setCommandWindow({ ...commandWindow, index: index });
+    let { left, top, pos, height } = position(ref.current);
+    //#region 가로이동 제한 & height 화면 안 넘어가게 제한
+    // 가로제한
+    if (left + 260 > SCREEN_WIDTH && left < 260) {
+      left = commandWindow.left;
+    } else if (SCREEN_WIDTH - left < 260 && left > 260) left -= 280;
+    // 세로제한
+    let heightCommand = 448;
+    top -= heightCommand;
+    if (top < 0) {
+      heightCommand += top - height;
+      top = 0;
     }
-    setPlaceHolder('명령어 사용시 "#"을 입력하세요.');
+    //#endregion
+    setCommandWindow({
+      visible: false,
+      top: top,
+      left: left,
+      index: index,
+      height: heightCommand,
+      pos: pos,
+    });
+
+    if (ref.current.innerHTML === "") setPlaceHolder('명령어 사용시 "#"을 입력하세요.');
   };
   return (
     <>
@@ -184,11 +183,7 @@ export const CTextarea = ({ style, maxrows, index }) => {
         suppressContentEditableWarning
         style={{ width: SCREEN_WIDTH - 60, color: GRAY7, outline: "none" }}
       />
-      {/* <div onInputCapture={(e)=>onChangeInput(e.target.innerText)} contentEditable suppressHydrationWarning style={{width:SCREEN_WIDTH - 60, outline:'none'}}>
-        <span style={{ color: "black" }} >asd</span>
-        <span style={{ color: "red" }}>asd</span>
-        {commandInputList[index].content}
-      </div> */}
+      <div style={{ color: GRAY6, fontSize: 15, position: "relative", height: 0, top: "-1.4em" }}>{placeholder}</div>
     </>
   );
 };
