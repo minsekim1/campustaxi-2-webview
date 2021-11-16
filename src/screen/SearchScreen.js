3889293cd345348b0564678b16169d0f91a9c4cd
Mins97 <tkarnrwl7862@naver.com>
2021-11-16 10:41:20 +0900

    backup

--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -3,12 +3,12 @@ import { BottomTabBar } from "../components/BottomTabBar";
 import { InputSearch } from "../components/Input";
 import { TitleHeader } from "../components/TitleHeader";
 
-
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import SwipeableViews from "react-swipeable-views";
 import useWindowDimensions from "../hook/useWindowDimensions";
-import { getfetch } from "../components/common";
+import { API_URL, getfetch } from "../components/common";
+import axios from "axios";
 
 const styles = {
   tabs: {
@@ -42,17 +42,48 @@ const SearchScreen = () => {
   );
 };
 
+const CancelToken = axios.CancelToken;
+const source = CancelToken.source();
+
 const SearchBar = () => {
   const { height, width } = useWindowDimensions();
   const filterSearchTxt = useRef();
 
-  //#region 네이버 상품검색
+  //#region 유저 / 채팅방 / 코스 검색
   const onChangeFilterSearchTxt = async (t) => {
+    // 검색시 이전꺼 멈춤
+    try {
+      if (source.cancel) source.cancel();
+    } catch (e) {
+      
+    } 
+
     if (t.length === 0) {
-      // setProcductInfo(procductInfoInit);
     } else {
       console.log("검색!");
-      getfetch("/users").then((d) =>console.log(d))
+      // 유저 닉네임 검색
+      axios.get(`${API_URL}/users?_q=${t}`, { cancelToken: source.token }).then(d=>console.log(d));
+      // fetch(`${API_URL}/users?_q=${t}`, {
+      //   method: "GET",
+      //   headers: {
+      //     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      //   },
+      // });
+      // 챗방 출/도 검색
+      // fetch(`${API_URL}/chat-rooms?_q=${t}`, {
+      //   method: "GET",
+      //   headers: {
+      //     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      //   },
+      // });
+      // // 코스 title 검색
+      // fetch(`${API_URL}/courses?_q=${t}`, {
+      //   method: "GET",
+      //   headers: {
+      //     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      //   },
+      // });
+
       // fetch(`/v1/search/shop.json?query=${t}&display=${procductInfoInit.display}`, {
       //   method: "GET",
       //   headers: {
