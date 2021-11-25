c7216959907de96ac77659e6206f9e44e992f379
MacBook <macbook@MacBookui-MacBookPro.local>
2021-11-25 15:13:04 +0900

    build upload

--- src/screen/CourseDetailScreen.tsx
+++ src/screen/CourseDetailScreen.tsx
@@ -292,7 +292,7 @@ const CommandArea = ({ content }: { content: CourseType[] }) => {
                 }}
               ></div>
               <div style={{ padding: 1.5 }}>
-                {JSON.stringify(item)}
+                {(item.id)}
                 {/* <CItem index={i} data={item} disable /> */}
               </div>
             </div>
